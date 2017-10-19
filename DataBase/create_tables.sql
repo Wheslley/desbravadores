c5e45e6d788621ca6a371af5e9e1e8944d2f367a
Wheslley <wheslleyny@gmail.com>
2017-10-19 01:19:04 -0200

    Script Banco de Dados

--- DataBase/create_tables.sql
+++ DataBase/create_tables.sql
@@ -25,3 +25,89 @@ create table usuarios(
     foreign key (id_permissao) references permissoes (id)
 );
 
+create table classes(
+	id int not null auto_increment,
+    nome varchar(100) not null,
+    idade int not null,
+    primary key (id)
+);
+
+create table pessoa_possui_classes(
+	id int not null auto_increment,
+    id_pessoa int not null,
+    id_classe int not null,
+    primary key (id),
+    foreign key (id_pessoa) references pessoa (id),
+    foreign key (id_classe) references classes (id)
+);
+
+create table especialidades(
+	id int not null auto_increment,
+    nome varchar(100) not null,
+    dificuldade varchar(50) not null,
+    primary key (id)
+);
+
+create table pessoa_possui_especialidade(
+	id int not null auto_increment,
+    id_pessoa int not null,
+    id_especialidade int not null,
+    data_especialidade date,
+    primary key (id),
+    foreign key (id_pessoa) references pessoa (id),
+    foreign key (id_especialidade) references especialidades (id)
+);
+
+create table clube(
+	id int not null auto_increment,
+    nome varchar(100) not null,
+    data_fundacao date not null,
+    primary key (id)
+);
+
+ALTER TABLE clube ADD regiao int;
+ALTER TABLE clube ADD id_cep int;
+ALTER TABLE clube ADD FOREIGN KEY (id_cep) REFERENCES cep (id);
+
+create table clube_possui_pessoa(
+	id int not null auto_increment,
+    id_clube int not null,
+    id_pessoa int not null,
+    primary key (id),
+    foreign key (id_clube) references clube (id),
+    foreign key (id_pessoa) references pessoa (id)    
+);
+
+create table cep(
+	id int not null auto_increment,
+	cidade varchar(100) not null,
+    estado varchar(80) not null,
+    pais varchar(50) not null,
+    primary key (id)
+);
+
+create table pessoa_possui_cep(
+	id int not null auto_increment,
+    id_pessoa int not null,
+    id_cep int not null,
+    primary key (id),
+    foreign key (id_pessoa) references pessoa (id),
+    foreign key (id_cep) references cep (id)
+);
+
+create table tipo_cargo(
+	id int not null auto_increment,
+    nome varchar(100) not null,
+    primary key (id)
+);
+
+create table tcargo_cep_pessoa(
+	id int not null auto_increment,
+    id_cargo int not null,
+    id_cep int not null,
+    id_pessoa int not null,
+    primary key(id),
+    foreign key (id_cargo) references tipo_cargo (id),
+    foreign key (id_cep) references cep (id),
+    foreign key (id_pessoa) references pessoa (id)    
+);
--- DataBase/insert_tables.sql
+++ DataBase/insert_tables.sql
@@ -1,3 +1,4 @@
+use desbravadores;
 
 /*permissoes*/
 insert into permissoes (descricao) values ('ADMINISTRADOR');
@@ -26,5 +27,151 @@ select u.usuario as usuario
   from usuarios u
      , permissoes p
  where u.id_permissao = p.id;*/
+
+insert into classes (nome, idade) values ('AMIGO',10);
+insert into classes (nome, idade) values ('AMIGO DA NATUREZA',10);
+insert into classes (nome, idade) values ('COMPANHEIRO',11);
+insert into classes (nome, idade) values ('COMPANHEIRO DE EXCURSIONISMO',11);
+insert into classes (nome, idade) values ('PESQUISADOR',12);
+insert into classes (nome, idade) values ('PESQUISADOR DE CAMPOS E BOSQUES',12);
+insert into classes (nome, idade) values ('PIONEIRO',13);
+insert into classes (nome, idade) values ('PIONEIRO DE NOVAS FRONTEIRAS',13);
+insert into classes (nome, idade) values ('EXCURCIONISTA',14);
+insert into classes (nome, idade) values ('EXCURCIONISTA DA MATA',14);
+insert into classes (nome, idade) values ('GUIA',15);
+insert into classes (nome, idade) values ('GUIA DE EXPLORAÇÃO',15);
+insert into classes (nome, idade) values ('LÍDER',16);
+insert into classes (nome, idade) values ('LÍDER MASTER',18);
+insert into classes (nome, idade) values ('LÍDER MASTER AVANÇADO',20);
+
+/*select * from classes;*/
+
+insert into pessoa_possui_classes (id_pessoa, id_classe) values (1,1);
+insert into pessoa_possui_classes (id_pessoa, id_classe) values (1,3);
+insert into pessoa_possui_classes (id_pessoa, id_classe) values (1,5);
+insert into pessoa_possui_classes (id_pessoa, id_classe) values (1,7);
+insert into pessoa_possui_classes (id_pessoa, id_classe) values (1,9);
+insert into pessoa_possui_classes (id_pessoa, id_classe) values (1,11);
+insert into pessoa_possui_classes (id_pessoa, id_classe) values (1,13);
+
+/*select * from pessoa_possui_classes;*/
+
+/*select p.nome
+     , c.nome
+     , c.idade
+  from pessoa_possui_classes ppc
+     , pessoa p
+	 , classes c
+where ppc.id_pessoa = p.id
+  and ppc.id_classe = c.id;*/
+
+insert into especialidades (nome, dificuldade) values ('Acampamento I','Fácil');
+insert into especialidades (nome, dificuldade) values ('Acampamento III','Fácil');
+insert into especialidades (nome, dificuldade) values ('Acampamento III','Médio');
+insert into especialidades (nome, dificuldade) values ('Acampamento IV','Médio');
+
+/*select * from especialidades;*/
+
+insert into pessoa_possui_especialidade (id_pessoa, id_especialidade, data_especialidade) values (1,1,now());
+insert into pessoa_possui_especialidade (id_pessoa, id_especialidade, data_especialidade) values (1,2,now());
+insert into pessoa_possui_especialidade (id_pessoa, id_especialidade, data_especialidade) values (1,3,now());
+insert into pessoa_possui_especialidade (id_pessoa, id_especialidade, data_especialidade) values (1,4,now());
+
+/*select * from pessoa_possui_especialidade;*/
+
+/*select p.nome
+     , e.nome
+     , e.dificuldade
+     , DATE_FORMAT(ppe.data_especialidade,'%d/%m/%Y') as data_especialidade
+  from pessoa_possui_especialidade ppe
+	 , pessoa p
+	 , especialidades e
+where ppe.id_pessoa = p.id
+  and ppe.id_especialidade = e.id;*/
+
+insert into clube (nome, data_fundacao) values ('Everest', now());
+update clube set regiao = 3 where id = 1;
+update clube set id_cep = 1 where id = 1;
+
+/*select * from clube;*/
+
+/*select c.nome
+     , c.regiao
+     , ce.cidade
+     , ce.estado
+     , ce.pais
+  from clube c
+     , cep ce
+where c.id_cep = ce.id;*/
+
+
+insert into clube_possui_pessoa (id_clube, id_pessoa) values (1,1);
+
+/*select * from clube_possui_pessoa;*/
+
+/*select c.nome
+     , p.nome
+  from clube_possui_pessoa cpp
+     , clube c
+     , pessoa p
+where cpp.id_clube = c.id
+  and cpp.id_pessoa = p.id;*/
   
- commit;
+insert into cep (cidade, estado, pais) values ('SÃO CARLOS','SÃO PAULO','BRASIL');
+
+/*select * from cep;*/
+
+insert into pessoa_possui_cep (id_pessoa, id_cep) values (1,1);
+
+/*select * from pessoa_possui_cep;*/
+
+/*select p.nome
+     , c.cidade
+     , c.estado
+     , c.pais
+  from pessoa_possui_cep ppc
+     , pessoa p
+     , cep c
+where ppc.id_pessoa = p.id
+  and ppc.id_cep = c.id;*/
+  
+insert into tipo_cargo (nome) values ('DIRETOR');
+insert into tipo_cargo (nome) values ('DIRETORA');
+insert into tipo_cargo (nome) values ('DIRETOR ASSOCIADO');
+insert into tipo_cargo (nome) values ('DIRETOR ASSOCIADA');
+insert into tipo_cargo (nome) values ('SECRETÁRIA');
+insert into tipo_cargo (nome) values ('SECRETÁRIO');
+insert into tipo_cargo (nome) values ('CAPELÃO');
+insert into tipo_cargo (nome) values ('INSTRUTOR DE ORDEM UNIDA');
+insert into tipo_cargo (nome) values ('INSTRUTORA DE ORDEM UNIDA');
+insert into tipo_cargo (nome) values ('INSTRUTOR DE ESPECIALIDADE');
+insert into tipo_cargo (nome) values ('INSTRUTORA DE ESPECIALIDADE');
+insert into tipo_cargo (nome) values ('CONSELHEIRO');
+insert into tipo_cargo (nome) values ('CONSELHEIRA');
+insert into tipo_cargo (nome) values ('INSTRUTOR');
+insert into tipo_cargo (nome) values ('INSTRUTORA');
+
+insert into tipo_cargo (nome) values ('DISTRITAL');
+insert into tipo_cargo (nome) values ('REGIONAL');
+insert into tipo_cargo (nome) values ('COORDENADOR');
+
+/*select * from tipo_cargo;*/
+
+insert into tcargo_cep_pessoa (id_cargo, id_cep, id_pessoa) values (16,1,1);
+
+/*select * from tcargo_cep_pessoa;*/
+
+/*select p.nome
+     , tc.nome
+     , c.cidade
+     , c.estado
+     , c.pais
+  from tcargo_cep_pessoa tcp
+  , tipo_cargo tc
+  , cep c
+  , pessoa p
+where tcp.id_cargo = tc.id
+  and tcp.id_cep = c.id
+  and tcp.id_pessoa = p.id;*/
+
+commit;
